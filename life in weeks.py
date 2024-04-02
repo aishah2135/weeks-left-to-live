bbf3b715827b221fb9361d8ca35ee0d93a5f60e8
aishah2135 <37914729+aishah2135@users.noreply.github.com>
2024-04-01 20:17:43 -0400

    Add files via upload

---
+++ life in weeks.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  5 16:29:20 2024
+
+@author: aisha
+"""
+print("Hello, what is your age?" )
+age = input()
+
+years = 90 - int(age)
+weeks = years * 52
+
+print(f"You have {weeks} weeks left.")
